3ec3d4fbdb3be6f8b84ae348c7b52012ce92fae0
gnegovetic <gnegovetic@hotmail.com>
2023-06-19 19:54:37 -0700

    Added files.

---
+++ hello_cuda.cpp
@@ -0,0 +1,32 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void HelloCuda() {
+    printf("Hello from GPU, block (%d,%d,%d), thread(%d,%d,%d)\n", 
+        blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z);
+}
+
+int main()
+{
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    auto cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        return -1;
+    }
+
+    HelloCuda<<<dim3(3,1,1), dim3(2,1,1)>>>();
+
+    printf("Hello from CPU\n");
+
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        return -1;
+    }
+
+    return 0;
+}
